af427e7511b4113f3f0bb39c52568e4f90a4a9e6
edisgipu <eagiraldo@sura.com.co>
2023-03-21 19:13:10 -0500

    Creacion de las pantallas funcionales

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  loginFormulario: FormGroup= this.fb.group({
+    email:    ['test1@cosa.com', [Validators.required, Validators.email]],
+    password: ['1234567', [Validators.required, Validators.minLength(6)]]
+  });
 
+  constructor( private fb: FormBuilder,
+                private router: Router) {}
+
+  login(){
+    console.log(this.loginFormulario.value);
+    //console.log(this.loginFormulario.valid);
+    
+    this.router.navigateByUrl('/dashboard');
+  }
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  usuarioNuevoFormulario: FormGroup= this.fb.group({
+    name:     ['Otro Usuario', [Validators.required, Validators.minLength(6)]],
+    email:    ['test1@cosa.com', [Validators.required, Validators.email]],
+    password: ['1234567', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private fb: FormBuilder,
+              private router: Router) {  }
+
+  craerUsuario(){
+    console.log(this.usuarioNuevoFormulario.value);
+    //console.log(this.usuarioNuevoFormulario.valid);
+
+    this.router.navigateByUrl('/dashboard');
+  }
 }
