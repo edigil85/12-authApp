a7ba8453b1f08a1ee0944363a82680103c525521
edisgipu <eagiraldo@sura.com.co>
2023-04-24 19:32:43 -0500

    Adicion de registro y mostrar email

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +12,28 @@ import { Router } from '@angular/router';
 export class RegisterComponent {
 
   usuarioNuevoFormulario: FormGroup= this.fb.group({
-    name:     ['Otro Usuario', [Validators.required, Validators.minLength(6)]],
+    name:     [' ', [Validators.required, Validators.minLength(6)]],
     email:    ['test1@cosa.com', [Validators.required, Validators.email]],
     password: ['1234567', [Validators.required, Validators.minLength(6)]]
   });
 
   constructor(private fb: FormBuilder,
-              private router: Router) {  }
+              private router: Router,
+              private authService: AuthService) {  }
 
   craerUsuario(){
     console.log(this.usuarioNuevoFormulario.value);
-    //console.log(this.usuarioNuevoFormulario.valid);
+    //console.log(this.loginFormulario.valid);
+    const {email, password, name} = this.usuarioNuevoFormulario.value
+
+    this.authService.registro(name, email, password)
+    .subscribe((ok: any)=>{
+      if (ok === true) {
+        this.router.navigateByUrl('/dashboard');
+      }else{
+        Swal.fire('Error', ok, 'error');
+      }
+    });
 
     this.router.navigateByUrl('/dashboard');
   }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -19,6 +19,27 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  registro (name: string, email: string, password: string){
+    const url=`${this.baseUrl}/auth/new`
+    const body = {name, email, password};
+
+    return this.http.post<AuthResponse>(url, body )
+    .pipe(
+      tap( resp=>{
+        if (resp.ok) {
+          localStorage.setItem('token', resp.token!);
+          this._usuario = {
+            name: resp.name!,
+            uid: resp.uid!,
+            email: resp.email!
+          }
+        }
+      } ),
+      map(resp=> resp.ok),
+      catchError( err=> of(err.error.msg))
+    ); 
+  }
+
   login(email: string, password: string){
     const url=`${this.baseUrl}/auth`
     const body = {email, password};
@@ -30,7 +51,8 @@ export class AuthService {
           localStorage.setItem('token', resp.token!);
           this._usuario = {
             name: resp.name!,
-            uid: resp.uid!
+            uid: resp.uid!,
+            email: resp.email!
           }
         }
       } ),
@@ -50,7 +72,8 @@ export class AuthService {
         localStorage.setItem('token', resp.token!);
           this._usuario = {
             name: resp.name!,
-            uid: resp.uid!
+            uid: resp.uid!,
+            email: resp.email!
           }
         return resp.ok
       }),
